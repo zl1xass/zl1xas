ee70280286d1d4a8b349f5136096b82d136f04f0
zl1xas <167357069+zl1xass@users.noreply.github.com>
2025-12-01 13:55:14 +0300

    Create plugins.lua

---
+++ plugins.lua
@@ -0,0 +1,17 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin" },
+
+  "neovim/nvim-lspconfig",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "L3MON4D3/LuaSnip",
+
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+
+  "nvim-tree/nvim-tree.lua",
+
+  "nvim-lualine/lualine.nvim",
+}
